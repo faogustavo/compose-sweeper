5835b7eb3c33590e00abe5c4ef0a1714e6352e89
Gustavo Valvassori <faogustavo@gmail.com>
2023-11-18 17:01:32 -0300

    reusing same instance for multiple games

--- src/jsMain/kotlin/dev/valvavassori/compose/sweeper/Main.kt
+++ src/jsMain/kotlin/dev/valvavassori/compose/sweeper/Main.kt
@@ -1,22 +1,18 @@
 package dev.valvavassori.compose.sweeper
 
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import dev.valvassori.theme.NineEightCSS
 import dev.valvavassori.compose.sweeper.core.MineSweeperBoard
 import dev.valvavassori.compose.sweeper.core.model.Difficulty
 import dev.valvavassori.compose.sweeper.ui.theme.MineSweeperCSS
-import kotlinx.coroutines.flow.MutableStateFlow
 import org.jetbrains.compose.web.css.Style
 import org.jetbrains.compose.web.renderComposable
 
 fun main() {
-    val game = MutableStateFlow(MineSweeperBoard(Difficulty.BEGINNER))
+    val gameInstance = MineSweeperBoard(Difficulty.BEGINNER)
+
     renderComposable(rootElementId = "root") {
         Style(NineEightCSS)
         Style(MineSweeperCSS)
-
-        val gameInstance by game.collectAsState()
 
         App(gameInstance)
     }
